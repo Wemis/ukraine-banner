3c5ba40b25f957e66acc0966d034b0792d7b37ab
Wemis <88230804+Wemis@users.noreply.github.com>
2022-10-30 00:41:47 +0200

    Update ukraine.sh

--- ukraine.sh
+++ ukraine.sh
@@ -1,6 +1,29 @@
 echo -e "clear" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo -e "date | lolcat" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo -e "figlet SLAWA UKRAINI | lolcat" /data/data/com.termux/files/usr/etc/bash.bashrc
-echo -e "" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-echo -e "" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e 'echo -e  "\033[0;34m
+                                   7~
+                                   ~+~
+                                  7=+=7
+                       7~7        ~+++~        7~
+                       7++~~7     ~+++~      :~++
+                       7++++~7    ~+++~    7~++++
+                       7+++~+:7   ~+++~    ~+~7++
+                       7+++7~+~   ~+++~   ~+~77++
+                       7+++ ~+~7  ~+++~  7~+~ 7++
+                       7+++  ~+~  7+++7  ~+~7 7++
+                       7+++  :+~  7~+~   ~+~  7++
+                       7+++  ~+~   ~+~   ~+~  7++
+                       7+++7~=+~77~+++~7 ~+=~ 7++        "' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e 'echo -e "                      \033[0;33m 7++~~+~~7~=+~I~++~7~~+~~++
+                       7+++++: ~++~  7~++~ :+++++
+                       7+++~++~++~     ~+=~++~7++
+                       7+++  ~~+~+++++++~+:~  7++
+                       7+++777~+~7~~+~~7~+~7777++
+                       7+++++++++++~+~+++++++++++
+                       7=======~~~=~+~=~~~=======
+                                ~+~~+~~+~7
+                                 ~+~+~+~7
+                                  ~~+~~
+                                    ~"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo -e "\033[0;32mDone!"
