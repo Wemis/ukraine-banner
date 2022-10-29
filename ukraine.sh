7a75c7fa4e6a73be93af4f964d8ab74bc0d1a853
Wemis <88230804+Wemis@users.noreply.github.com>
2022-10-30 00:38:44 +0200

    Create ukraine.sh

---
+++ ukraine.sh
@@ -0,0 +1,6 @@
+echo -e "clear" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e "date | lolcat" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e "figlet SLAWA UKRAINI | lolcat" /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e "" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e "" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e "\033[0;32mDone!"
